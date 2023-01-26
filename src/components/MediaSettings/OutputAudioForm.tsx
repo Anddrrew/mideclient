a13b3e266b3c5db7dd1db4ac293350a1ce2f079d
Anddrrew <59182007+Anddrrew@users.noreply.github.com>
2023-01-26 22:16:18 +0200

    Added OutputAudioForm

--- src/components/MediaSettings/OutputAudioForm.tsx
+++ src/components/MediaSettings/OutputAudioForm.tsx
@@ -1,14 +1,57 @@
-import { Stack, Typography } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
 import AudioDeviceForm from './AudioDeviceForm';
 import { useDevices } from '../../contexts/DevicesContext';
+import bleepAudio from './../../assets/audios/bleep.mp3';
+import { useEffect, useState } from 'react';
+
+/* setSinkId doesn't work for mobile devices.
+ * https://caniuse.com/mdn-api_htmlmediaelement_setsinkid
+ */
+
+interface AudioElement extends HTMLVideoElement {
+  setSinkId(id: string): void;
+}
 
 export default function OutputAudioForm() {
   const { audioOutput } = useDevices();
+  const [volume, setVolume] = useState(100);
+  const [deviceId, setDeviceId] = useState('default');
+  const [audio, setAudio] = useState(new Audio(bleepAudio) as AudioElement);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleDeviceIdChange = (deviceId: string) => setDeviceId(deviceId);
+  const handleVolumeChange = (volume: number) => setVolume(volume);
+  const handleCheck = () => {
+    setIsPlaying((s) => !s);
+    audio.play();
+    setTimeout(() => setIsPlaying((s) => !s), audio.duration * 1000);
+  };
+
+  useEffect(() => {
+    setAudio((audio) => {
+      audio.volume = volume / 100;
+      return audio;
+    });
+  }, [volume]);
+
+  useEffect(() => {
+    setAudio((audio) => {
+      audio.setSinkId(deviceId);
+      return audio;
+    });
+  }, [deviceId]);
 
   return (
     <Stack>
       <Typography variant='subtitle1'>Output Device</Typography>
-      <AudioDeviceForm devices={audioOutput} />
+      <AudioDeviceForm
+        devices={audioOutput}
+        onDeviceChange={handleDeviceIdChange}
+        onVolumeChange={handleVolumeChange}
+      />
+      <Button onClick={handleCheck} variant='contained' disabled={isPlaying}>
+        Check
+      </Button>
     </Stack>
   );
 }
